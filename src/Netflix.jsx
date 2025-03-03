72f9f5cb45cafaa8be0320b70c9c2f8282314fa1
AkanshaMittal <akanshamittal210@gmail.com>
2025-03-03 11:55:49 +0530

    Initial commit

---
+++ src/Netflix.jsx
@@ -0,0 +1,31 @@
+const Netflix =()=>{
+    const Genre=()=>{
+        const Genre="Super natural power"
+        return Genre;
+    }
+return(
+    <div>
+        <div>
+            <img src="MoneyHeist.jpg" alt="wednesday" width="40%" height="40%"/>
+        </div>
+        <h2>Name:</h2>
+        
+        <h3>Rating:</h3>
+        <p>Description:</p>
+        <p>Genre:{Genre()}</p>
+
+    </div>
+)
+}
+
+
+
+
+
+
+
+
+
+
+
+export default Netflix;
